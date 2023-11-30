de6b29e31e6df36879125bc344e05ae323d50214
diana <dinazhusubalieva@gmail.com>
2023-11-30 13:35:15 +0600

    add global style variables, dark/light themes logic

---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,24 @@
+import React, {useMemo, useState} from 'react';
+import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from "./ThemeContext";
+
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
+
+interface Props{
+    children: React.ReactNode;
+}
+const ThemeProvider: React.FC<Props> = ({children}) => {
+    const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+
+    const defaultProps = useMemo(() => ({
+        theme, setTheme
+    }), [theme])
+
+    return (
+        <ThemeContext.Provider value={defaultProps}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export default ThemeProvider;
